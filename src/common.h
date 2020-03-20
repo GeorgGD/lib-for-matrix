18f6b8bfbb510d1577243c14ead716f410e12eca
GeorgGD <giorgos_davak@hotmail.com>
2020-03-20 23:59:41 +0100

    Add documentation

--- src/common.h
+++ src/common.h
@@ -1,6 +1,13 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+/**
+ * @file common.h
+ * @author Georgios Davakos 
+ * @date 21 mar 2020
+ * @brief Shared data structures
+ */
+
 typedef struct matrix matrix_t;
 
 struct __attribute__((__packed__)) matrix
