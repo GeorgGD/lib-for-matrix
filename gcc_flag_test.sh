837c3be3d7d97cc461bc2c3a6bc1ed7e99dbe2d3
GeorgGD <giorgos_davak@hotmail.com>
2020-03-22 11:27:58 +0100

    Improve the output text from gcc_flag_test

--- gcc_flag_test.sh
+++ gcc_flag_test.sh
@@ -4,7 +4,7 @@ make -s clean
 echo ==========================================================
 echo Flags = None
 make 
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = None/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
@@ -12,7 +12,7 @@ echo
 echo ==========================================================
 echo Flags = -O1 -march=native
 make -s OFLAGS="-O1 -march=native" 
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = O1/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
@@ -20,7 +20,7 @@ echo
 echo ==========================================================
 echo Flags = -O2 -march=native
 make -s OFLAGS="-O2 -march=native"
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = O2/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
@@ -28,7 +28,7 @@ echo
 echo ==========================================================
 echo Flags = -O3 -march=native
 make -s OFLAGS="-O3 -march=native"
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = O3/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
@@ -36,7 +36,7 @@ echo
 echo ==========================================================
 echo Flags = -Os -march=native
 make -s OFLAGS="-Os -march=native "
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = Os/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
@@ -44,7 +44,8 @@ echo
 echo ==========================================================
 echo Flags = -O3 -march=native -ffast-math
 make -s OFLAGS="-O3 -march=native -ffast-math"
-time ./lu 1500 0
+{ time (./lu 1500 0) >/dev/null; } 2>&1 | grep real | cut -f 2 | cut -c 3- | sed "s/$/ = O3 with ffast-math/" >> flag_test.txt
 make -s clean
 echo ==========================================================
 echo
+
