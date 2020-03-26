f095311d19edc5dacd8fcf9480b116e486a4372b
GeorgGD <giorgos_davak@hotmail.com>
2020-03-26 23:15:50 +0100

    Added tests for determinant function

--- test/unittest.c
+++ test/unittest.c
@@ -26,9 +26,9 @@ void test_create_matrix()
   matrix_t *matrix = create_empty_matrix(size);
   random_matrix(matrix);
   CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 0), 4.0);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 1), 2.0);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 0), 3.0);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 1), 1.0);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 1), -2.0);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 0), -2.0);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 1), 2.0);
   
   destroy_matrix(matrix);
 }
@@ -40,9 +40,9 @@ void test_lu_factor()
   random_matrix(matrix);
   lu_factor(matrix, 1);
   CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 0), 4.0);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 1), 0.5);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 0), 3.0);
-  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 1), -0.5);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 0, 1), -0.5);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 0), -2.0);
+  CU_ASSERT_EQUAL(inspect_matrix(matrix, 1, 1), 1.0);
   
   destroy_matrix(matrix);
 }
@@ -54,10 +54,10 @@ void test_upper_matrix()
   random_matrix(matrix);
   lu_factor(matrix, 1);
   matrix_t * upper_matrix = create_upper_matrix(matrix);
-  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 0, 0), 16.0);
-  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 0, 1), 0);
-  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 1, 1), -0.25);
-  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 2, 1), -0.5);
+  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 0, 0), 4.0);
+  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 0, 1), -0);
+  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 1, 1), 3.0);
+  CU_ASSERT_EQUAL(inspect_matrix(upper_matrix, 2, 1), -2.0);
   
   destroy_matrix(matrix);
   destroy_matrix(upper_matrix);
@@ -71,13 +71,30 @@ void test_lower_matrix()
   lu_factor(matrix, 1);
   matrix_t * lower_matrix = create_lower_matrix(matrix);
   CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 0, 0), 1.0);
-  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 0, 1), 0.75);
+  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 0, 1), -0.5);
   CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 1, 1), 1.0);
-  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 1, 2), 2.0);
-  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 1, 3), 3.0);
+  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 1, 2), -2.0/3.0);
+  CU_ASSERT_EQUAL(inspect_matrix(lower_matrix, 1, 3), 0);
 
   destroy_matrix(matrix);
   destroy_matrix(lower_matrix);
+}
+
+void test_determinant()
+{
+  int size = 4;
+  matrix_t *matrix = create_empty_matrix(size);
+  random_matrix(matrix);
+  int det = determinant(matrix);
+  CU_ASSERT_EQUAL(det, 16);
+  destroy_matrix(matrix);
+
+  size = 3;
+  matrix = create_empty_matrix(size);
+  random_matrix(matrix);
+  det = determinant(matrix);
+  CU_ASSERT_EQUAL(det, 8);
+  destroy_matrix(matrix);
 }
 
 int main()
@@ -98,8 +115,8 @@ int main()
       (NULL == CU_add_test(test_suite1, "Test create matrix", test_create_matrix)) ||
       (NULL == CU_add_test(test_suite1, "Test LU factorization", test_lu_factor)) ||
       (NULL == CU_add_test(test_suite1, "Test upper matrix", test_upper_matrix)) ||
-      (NULL == CU_add_test(test_suite1, "Test lower matrix", test_lower_matrix)) 
-
+      (NULL == CU_add_test(test_suite1, "Test lower matrix", test_lower_matrix)) ||
+      (NULL == CU_add_test(test_suite1, "Test determinant", test_determinant))
       )
     {
       CU_cleanup_registry();
